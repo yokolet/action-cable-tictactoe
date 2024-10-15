3f95599fa9add4e100b459cede1e95c369705b90
Yoko Harada <yokolet@gmail.com>
2024-10-15 11:31:08 +0900

    changes the key from action to act since action is overrided by Rails

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,14 +1,9 @@
 # frozen_string_literal: true
 
 class Player
-  attr_reader :name, :player_of
+  attr_reader :name
 
   def initialize(name)
     @name = name
-    @player_of = []
-  end
-
-  def join(board_id)
-    @player_of << board_id
   end
 end
